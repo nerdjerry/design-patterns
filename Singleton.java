500bc91a3d858a8f1628c3ff49b71935ccbeadc1
lucifer <psinghal4210@gmail.com>
2021-03-17 21:40:00 +0530

    Singleton

---
+++ Singleton.java
@@ -0,0 +1,13 @@
+public class Singleton{
+	private static Singleton firstInstance = null;
+
+	private Singleton()
+
+	public static Singleton getInstance(){
+		if(firstInstance == null){
+			firstInstance = new Singleton();
+		}
+
+		return firstInstance;
+	}
+}
